1aa2f4ddbc6a9d50cebe089c6ebafb0adad30ca4
i-b8o <105774619+i-b8o@users.noreply.github.com>
2022-12-18 20:38:13 +0300

    Update regulation.go
    Added Todo

--- internal/controllers/http/v1/regulation.go
+++ internal/controllers/http/v1/regulation.go
@@ -64,6 +64,7 @@ type Data struct {
 	Updated      string
 }
 
+// TODO center the logo for mobile 
 func (h *regulationHandler) Home(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	h.templateManager.RenderTemplate(w, "home", nil)
 }
